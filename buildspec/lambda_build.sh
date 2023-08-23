79f9cae98db435e189c9b521fc5389e68d0e4ae7
Jason E. Robinson <jason.robinson@auspex-labs.com>
2023-08-23 01:59:28 -0400

    Update Lambda Build

--- buildspec/lambda_build.sh
+++ buildspec/lambda_build.sh
@@ -6,15 +6,16 @@
 fil1=./lambda/
 fil2=.py
 
+zip -qr9 function.zip ./components
+
 for lambda in ./lambda/*.py
 do
   dir=$(printf '%s' "${lambda//$fil1/}")
   dir=$(printf '%s' "${dir//$fil2/}")
 
-  if [ "$dir" != "__init__" ]
+  if [ $dir != "__init__" ]
   then
-    mkdir "$dir" "$dir"/lambda
-    mv "$lambda" "$dir"/"$lambda"
-    cp ./components "$dir"/components -r
+    cp function.zip $dir.zip
+    zip -qr9 $dir.zip $lambda
   fi
 done
